de3515efbe2329a894efe434218e5341244a8931
Ignacio Arias <nacho.mambo@gmail.com>
2021-08-27 16:55:52 -0300

    incluiyendo shaders en script

--- assets/actualizador.bat
+++ assets/actualizador.bat
@@ -5,28 +5,44 @@ echo.
 echo.
 
 echo           Actualizador de MODS
-echo              Version 2.3
+echo              Version 2.4
 echo.
 echo.
 
 echo Desea descargar los nuevos MODS? [S/N] (luego enter)
-echo.>MiPregunta.$$$
-DEL MiPregunta.$$$ /P > NUL
-if exist MiPregunta.$$$ goto No
+echo.>MODSVAR.$$$
+DEL MODSVAR.$$$ /P > NUL
+if exist MODSVAR.$$$ goto No
 
-cls
-echo Comenzando....
-rd /s /q "%~d0%~p0\mods" /s
-taskkill /IM javaw.exe /F
-del  "%userprofile%\AppData\Roaming\.minecraft\mods\*.jar"
-cls
-git clone https://github.com/Soulote/mods.git
-xcopy "%~d0%~p0\mods\*.*" "%userprofile%\AppData\Roaming\.minecraft\mods" /y
-rd /s /q "%~d0%~p0\mods" /s
+    cls
+    echo Comenzando....
+    rd /s /q "%~d0%~p0\mods" /s
+    taskkill /IM javaw.exe /F
+    del  "%userprofile%\AppData\Roaming\.minecraft\mods\*.jar"
+    cls
+    git clone https://github.com/Soulote/mods.git
+    xcopy "%~d0%~p0\mods\*.*" "%userprofile%\AppData\Roaming\.minecraft\mods" /y
+    rd /s /q "%~d0%~p0\mods" /s
+    cls
 
+    echo Desea incluir el Mod para Shaders? [S/N] (luego enter)
+    echo.>SHADERS.$$$
+    DEL SHADERS.$$$ /P > NUL
+    if exist SHADERS.$$$ goto No
+        echo.
+        echo.
+        echo Incluyendo Shaders....
+    goto Fin
+    :No
+        del SHADERS.$$$
+        echo.
+        echo.
+        echo Eliminando Shaders....
+        del "%userprofile%\AppData\Roaming\.minecraft\mods\iris_shaders.jar"
+    :Fin
 goto Fin
 :No
-del MiPregunta.$$$
+del MODSVAR.$$$
 cls
 echo.
 echo.
