193707088f6ecbc263ead8b4ba0deba063af04c3
Ignacio Arias <nacho.mambo@gmail.com>
2021-08-27 16:14:28 -0300

    bat mejorado

--- assets/actualizador.bat
+++ assets/actualizador.bat
@@ -1,18 +1,39 @@
 @echo off
 
-::https://git-scm.com/downloads
+color 03
+echo.
+echo.
 
+echo           Actualizador de MODS
+echo              Version 2.3
+echo.
+echo.
+
+echo Desea descargar los nuevos MODS? [S/N] (luego enter)
+echo.>MiPregunta.$$$
+DEL MiPregunta.$$$ /P > NUL
+if exist MiPregunta.$$$ goto No
+
+cls
+echo Comenzando....
 rd /s /q "%~d0%~p0\mods" /s
 taskkill /IM javaw.exe /F
-cls
 del  "%userprofile%\AppData\Roaming\.minecraft\mods\*.jar"
+cls
 git clone https://github.com/Soulote/mods.git
-timeout /t 1
 xcopy "%~d0%~p0\mods\*.*" "%userprofile%\AppData\Roaming\.minecraft\mods" /y
-
 rd /s /q "%~d0%~p0\mods" /s
 
+goto Fin
+:No
+del MiPregunta.$$$
 cls
+echo.
+echo.
+echo Cancelando proceso....
+:Fin
+echo.
+echo.
 
 echo /////////////////////////////////////////////////////////////////
 echo /////////////////////////////////////////////////////////////////
